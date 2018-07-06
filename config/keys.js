5e7863e19c2cd68e557a4434a7224fa43e7ed61d
sumathi <sumathi@rocketmail.com>
2018-07-06 02:41:03 -0400

    user, register, login, edit api routes completed

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGODB_URI:'mongodb://sg2018cc:sg2018cc@ds125851.mlab.com:25851/communityconnector'
+    MONGODB_URI:'mongodb://sg2018cc:sg2018cc@ds125851.mlab.com:25851/communityconnector',
+    secretOrKey:'secret'
 }
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
+const bodyParser=require('body-parser')
+const passport = require('passport')
+
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 //Database Config
 const db = require("./config/keys").MONGODB_URI;
 
@@ -17,11 +21,21 @@ mongoose
   .then(() => console.log(`MongoDatabase Connected .......`))
   .catch(err => console.log(err));
 
+// passport middleware
+app.use(passport.initialize())
+
+//Passport JWT authentication strategy
+require('./config/passport')(passport)
+
 //Use Routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    res.json("Hello................")
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on Port: ${PORT}......`);
