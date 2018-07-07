374bccc63ee2bdcf6e004b58a8b2e2907b124f53
sumathi <sumathi@rocketmail.com>
2018-07-07 08:13:48 -0400

    prepared to deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const bodyParser=require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 
 const app = express();
@@ -30,6 +31,14 @@ require('./config/passport')(passport)
 //Use Routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
+
+//serve static assets in production
+if(process.env.NODE_ENV==="production") {
+  app.use(express.static('client/build'))
+  app.get('*', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
